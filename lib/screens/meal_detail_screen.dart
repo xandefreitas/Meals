1de3ad2ffec4de1de0161143daa093ed640ea4f9
Alexandre Freitas <xande_liborio@hotmail.com>
2022-03-31 09:55:23 -0300

    196. Tela Detalhe Refeição 01

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -11,8 +11,47 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Center(
-        child: Text('Detalhes da Refeição'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.maxFinite,
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text(
+              'Ingredientes',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          Container(
+            height: 250,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: EdgeInsets.all(8),
+            margin: EdgeInsets.all(8),
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (ctx, i) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(meal.ingredients[i]),
+                  ),
+                  color: Theme.of(context).colorScheme.secondary,
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
